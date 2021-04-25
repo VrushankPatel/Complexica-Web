32cc43cb04fa0d0f8ac548969b4e6addc1f68c10
Vrushank Patel <vrushankpatel@Vrushanks-MacBook-Air.local>
2021-04-25 14:09:25 +0530

    chhery pick from CNavBar branch

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import CNavBar from "../components/cNavBar";
-class WelcomeView extends Component {
+
+class AboutView extends Component {
   state = {};
   render() {
     return <CNavBar page="about" />;
   }
 }
 
-export default WelcomeView;
+export default AboutView;
