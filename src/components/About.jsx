57ff97404efd8a8c95a84de31985e0fa16386424
Vrushank Patel <vrushankpatel5@gmail.com>
2021-05-16 09:25:41 +0530

    added about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,35 +5,51 @@ class Body extends Component {
   render() {
     const txtStyle = {
       fontSize: "xxx-large",
-      fontWeight: "900",
+      fontWeight: "90",
       color: "#363634",
-      fontFamily: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",
+      fontFamily: "Exo",
       // paddingTop: "220px",
       // paddingLeft: "50px",
     };
+    const subTxtStyle = {
+      fontSize: "xx-large",
+      fontWeight: "90",
+      color: "#363634",
+      fontFamily: "Exo",
+      // paddingTop: "220px",
+      // paddingLeft: "50px",
+    };
+    const colStyle = {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }
 
     return (
       <Container>
         <Row className="justify-content-center">
           <Col
             lg={12}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+            style={colStyle}
           >
-
             <Row>
               <h3
                 className="pt-5"
                 style={txtStyle}
               >
-                Complexica v1
+                Complexica v1.0.0
               </h3>
             </Row>
-
-
+          </Col>
+          <Col
+            lg={12}
+            style={colStyle}
+          >
+            <Row>
+              <h3 style={subTxtStyle}>
+                Developer : Vrushank Patel
+              </h3>
+            </Row>
           </Col>
           {/* <Col
             lg={4}
